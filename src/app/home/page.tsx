9f761dd6753f97d610959d5452a928f15aafe666
Armin Jazi <a.ghajarjazi@reply.de>
2025-01-03 17:33:04 +0100

    fix an issue with the background

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className='relative'>
       <div
-        className="h-full bg-cover bg-center bg-fixed z-[-1] bg-no-repeat absolute top-0 left-0 right-0 fixed"
+        className="h-full bg-cover bg-center bg-fixed z-[-1] bg-no-repeat top-0 left-0 right-0 fixed"
         style={{ backgroundImage: "url('/background.png')", backgroundAttachment: "fixed" }}
       />
       <SwitchModes />
